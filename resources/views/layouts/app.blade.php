0af9388d4e3442d5355d8fdd9c04258b21ab5453
seagame <seagame@seagames-MacBook-Pro.local>
2017-06-13 15:47:00 +0700

    finish?

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        {{ config('Task List', 'Task List') }}
                     </a>
                 </div>
 
